fcd039f0cd9c6211a0706b50dd458038c840f92c
tanpv <phamtan500@gmail.com>
2017-02-03 17:28:32 +0700

    Update ActivateLicense.cpp

--- SdkAutoTest/ActivateLicense/ActivateLicense.cpp
+++ SdkAutoTest/ActivateLicense/ActivateLicense.cpp
@@ -34,27 +34,27 @@ std::string edfFile = "../../edf/t.edf";
 // case EDK_LICENSE_NOT_FOUND
 std::string const LICENSE_KEY_NOT_FOUND = "11b9d092-db6b-4f18-b7b4-04d640110d4c";
 //case EDK_LICENSE_ERROR
-std::string const LICENSE_KEY_ERROR = "50541929-7330-4059-b073-30814ff15ea2";
+std::string const LICENSE_KEY_ERROR = "1a3ba5f0-2554-42de-a261-7b777e7640f7";
 // case EDK_LICENSE_EXPIRED
 std::string const LICENSE_KEY_EXPIRED = "a1b9d092-db6b-4f18-b7b4-04d640110d4c";
 //case EDK_LICENSE_REGISTERED
-std::string const LICENSE_KEY_VALID = "50541929-7330-4059-b073-30814ff15ea2";
+std::string const LICENSE_KEY_VALID = "1a3ba5f0-2554-42de-a261-7b777e7640f7";
 //case EDK_LICENSE_DEVICE_LIMITED
 std::string const LICENSE_KEY_DEVICE_LIMITED = "5e9f73e0-3aab-4bc7-9477-9838c3c0eb23";
 //case EDK_UNKNOWN_ERROR
-std::string const UNKNOWN_ERROR = "50541929-7330-4059-b073-30814ff15ea2";
+std::string const UNKNOWN_ERROR = "1a3ba5f0-2554-42de-a261-7b777e7640f7";
 //case EDK_OVER_QUOTA_IN_DAY
-std::string const LICENSE_KEY_OVER_QUOTA_IN_DAY = "50541929-7330-4059-b073-30814ff15ea2";
+std::string const LICENSE_KEY_OVER_QUOTA_IN_DAY = "1a3ba5f0-2554-42de-a261-7b777e7640f7";
 //case EDK_OVER_QUOTA_IN_MONTH
-std::string const LICENSE_KEY_OVER_QUOTA_IN_MONTH = "50541929-7330-4059-b073-30814ff15ea2";
+std::string const LICENSE_KEY_OVER_QUOTA_IN_MONTH = "1a3ba5f0-2554-42de-a261-7b777e7640f7";
 //case EDK_OVER_QUOTA
-std::string const LICENSE_KEY_OVER_QUOTA = "50541929-7330-4059-b073-30814ff15ea2";
+std::string const LICENSE_KEY_OVER_QUOTA = "1a3ba5f0-2554-42de-a261-7b777e7640f7";
 //case EDK_ACCESS_DENIED
-std::string const LICENSE_KEY_ACCESS_DENIED = "50541929-7330-4059-b073-30814ff15ea2";
+std::string const LICENSE_KEY_ACCESS_DENIED = "1a3ba5f0-2554-42de-a261-7b777e7640f7";
 //case EDK_LICENSE_ERROR
-std::string const LICENSE_KEY_LICENSE_ERROR = "50541929-7330-4059-b073-30814ff15ea2";
+std::string const LICENSE_KEY_LICENSE_ERROR = "1a3ba5f0-2554-42de-a261-7b777e7640f7";
 //case EDK_NO_ACTIVE_LICENSE
-std::string const LICENSE_KEY_NO_ACTIVE_LICENSE = "50541929-7330-4059-b073-30814ff15ea2";
+std::string const LICENSE_KEY_NO_ACTIVE_LICENSE = "1a3ba5f0-2554-42de-a261-7b777e7640f7";
 
 BOOST_AUTO_TEST_SUITE(ACTIVE_LICENSE)
 
@@ -93,7 +93,6 @@ BOOST_AUTO_TEST_CASE(TC2_GIVEN_have_a_valid_license_WHEN_server_is_up_THEN_user_
 // Use license for 5 times, so license run out of quota
 // Connect to emoengine
 // Expect the return is over quota in day
-
 BOOST_AUTO_TEST_CASE(TC3_GIVEN_have_a_valid_license_with_1_seat_WHEN_after_use_5_sessions_THEN_user_should_get_message_run_out_of_quota) {
 	
 	int result;
@@ -117,7 +116,8 @@ BOOST_AUTO_TEST_CASE(TC3_GIVEN_have_a_valid_license_with_1_seat_WHEN_after_use_5
 	// Get license info
 	IEE_LicenseInfos_t licenseInfos;
 	IEE_LicenseInformation(&licenseInfos);
-	BOOST_TEST_MESSAGE(licenseInfos.usedQuotaDay);
+	BOOST_TEST_MESSAGE("Used in a day: " << (int)licenseInfos.usedQuotaDay);
+	BOOST_TEST_MESSAGE("Number of seat: " << (int)licenseInfos.seat_count);
 	int run_until_out_of_day_quota = 5 - licenseInfos.usedQuotaDay;
 
 	for (int i = 0; i < run_until_out_of_day_quota; i++)
@@ -130,51 +130,12 @@ BOOST_AUTO_TEST_CASE(TC3_GIVEN_have_a_valid_license_with_1_seat_WHEN_after_use_5
 	}
 
 	// connect to engine after out of quota
+	int current_used_session = licenseInfos.usedQuota;
 	IEE_EngineConnect();
-	boost::this_thread::sleep(boost::posix_time::seconds(61));
+	boost::this_thread::sleep(boost::posix_time::seconds(65));
 	IEE_EngineDisconnect();
 	// used quota not increase after run out of quota
-	BOOST_CHECK(licenseInfos.usedQuota == 5);
-
+	BOOST_CHECK(licenseInfos.usedQuotaDay == 5);
 }
-
-
-
-BOOST_AUTO_TEST_CASE(TC4_GIVEN_have_a_LICENSE_KEY_OVER_QUOTA_IN_MONTH_WHEN_server_is_up_THEN_user_could_not_activate_license) {
-	int result;
-	result = IEE_ActivateLicense(LICENSE_KEY_OVER_QUOTA_IN_MONTH.c_str());
-	BOOST_CHECK(result== EDK_OVER_QUOTA_IN_MONTH);
-	IEE_LicenseInfos_t licenseInfos;
-	// We can call this API any time to check current License information
-	result = IEE_LicenseInformation(&licenseInfos);
-	BOOST_CHECK(result== EDK_OVER_QUOTA_IN_MONTH);
-	if (result==EDK_OK) {
-		BOOST_TEST_MESSAGE("Quota limit in month: "<< (int)licenseInfos.quotaMonthLimit);
-		BOOST_TEST_MESSAGE("Quota used in month : " << (int)licenseInfos.usedQuotaMonth);
-	}
-		//BOOST_TEST_MESSAGE("Total Quota Remain in day: " + remainQuotaMonth);
-	}
-}
-
-BOOST_AUTO_TEST_CASE(TC5_GIVEN_have_a_LICENSE_KEY_OVER_QUOTA_WHEN_server_is_up_THEN_user_could_not_activate_license) {
-	int result;
-	result = IEE_ActivateLicense(LICENSE_KEY_OVER_QUOTA.c_str());
-	BOOST_CHECK_EQUAL(result, EDK_OVER_QUOTA);
-	
-	
-}
-BOOST_AUTO_TEST_CASE(TC6_GIVEN_have_a_LICENSE_KEY_DEVICE_LIMITED_WHEN_server_is_up_THEN_user_could_not_activate_license) {
-	int result;
-	result = IEE_ActivateLicense(LICENSE_KEY_DEVICE_LIMITED.c_str());
-	BOOST_CHECK_EQUAL(result, EDK_LICENSE_DEVICE_LIMITED);
-
-}
-BOOST_AUTO_TEST_CASE(TC7_GIVEN_have_a_LICENSE_KEY_NOT_FOUND_WHEN_server_is_up_THEN_user_could_not_activate_license) {
-	int result;
-	result = IEE_ActivateLicense(LICENSE_KEY_NOT_FOUND.c_str());
-	BOOST_CHECK_EQUAL(result, EDK_LICENSE_NOT_FOUND);
-
-}
-
 
 
